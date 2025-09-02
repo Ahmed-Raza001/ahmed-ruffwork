34d66c55b71407b8da2f59d5a31564eb19d3ab84
Ahmed-Raza001 <ahmadrazaharsoliya67@gmail.com>
2025-09-02 07:12:28 +0000

    im x xp

---
+++ one.js
@@ -0,0 +1,7 @@
+/*export const greeting = "Hello, Odinite!";
+export const farewell = "Bye bye, Odinite!";*/
+
+//export default "Hello, Odinite!";
+
+const greeting = "Hello, Odinite!";
+export default greeting;
---
+++ two.js
@@ -0,0 +1,8 @@
+/*import { greeting, farewell } from "./script.js";
+
+console.log(greeting); // "Hello, Odinite!"
+console.log(farewell); // "Bye bye, Odinite!"*/
+
+import helloOdinite from "./one.js";
+
+console.log(helloOdinite); 
